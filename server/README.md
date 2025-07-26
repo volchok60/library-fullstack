465630fab3dd6bbe719b3a7a4014ceee2b0658e2
Volodymyr Cholak <volchok872@gmail.com>
2025-07-26 13:02:36 -0600

    Updated README.md

---
+++ server/README.md
@@ -0,0 +1,30 @@
+# Server Backend
+
+Biult with FastAPI.
+
+## Setup 
+
+1. Scaffold app
+
+```sh
+uv init server
+```
+
+2. Add packages
+
+```sh
+uv add ruff
+uv add fastapi --extra standard
+```
+
+3. Start local development
+
+```sh
+uv run fastapi dev
+```
+
+## Linting and formatting
+
+```sh
+uv run ruff check
+```
--- server/main.py
+++ server/main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from server!")
+from enum import Enum
+from typing import Annotated
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
+# Python enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
-if __name__ == "__main__":
-    main()
+class Item(BaseModel):
+    name: str
+    description: str | None = None # optional
+    price: float
+    tax: float | None = None # optional
+
+app = FastAPI()
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50)] = None): # q is an optional query parameter
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
+
