cc274c295e7c0e6800f91028b339d01577ca9707
Volodymyr Cholak <volchok872@gmail.com>
2025-07-28 13:37:04 -0600

    Initial migration.

--- server/alembic/versions/159d0802ef2f_added_user_model.py
+++ server/alembic/versions/159d0802ef2f_added_user_model.py
@@ -1,18 +1,19 @@
 """Added User model
 
-Revision ID: 72a7f129a139
+Revision ID: 159d0802ef2f
 Revises: 
-Create Date: 2025-07-28 13:19:46.570038
+Create Date: 2025-07-28 13:36:05.668744
 
 """
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
 
 
 # revision identifiers, used by Alembic.
-revision: str = '72a7f129a139'
+revision: str = '159d0802ef2f'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
